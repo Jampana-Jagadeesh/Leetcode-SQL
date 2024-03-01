e7a6858402b25fd18402558e13fbfe759ad2ee08
Jagadeesh <jagadeeshjampana5@gmail.com>
2024-03-01 23:09:18 +0530

    Fourth

---
+++ 4. Sorting and Grouping/596. Classes More Than 5 Students/Solution.sql
@@ -0,0 +1,3 @@
+select class from Courses
+group by class
+having count(distinct student)>=5;
---
+++ 4. Sorting and Grouping/619. Biggest Single Number/Solution.sql
@@ -0,0 +1,3 @@
+select  max(num) as num from 
+(select num from mynumbers group by num 
+having count(*)=1) as fun
